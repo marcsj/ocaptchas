a65b3b94408770fc3e8ef6351e0c29a4fa8e4ca4
marcsj <marc@marcsj.com>
2020-03-22 12:11:11 -0700

    Added basic setup for database and session repo in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,20 @@
 package server
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/marcsj/ocaptchas/repo"
+	"log"
+)
+
+func main() {
+	//TODO: change where this database is located by an arg
+	db, err := gorm.Open("sqlite3", "stored.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = repo.NewSessionRepo(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
