9eabe03583e81b4f26795c7c03c998274b9489b8
marcsj <marc@marcsj.com>
2020-03-28 22:04:39 -0700

    Added grpc server startup and main server setups.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,70 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/marcsj/ocaptchas/challenge"
+	"github.com/marcsj/ocaptchas/controller"
 	"github.com/marcsj/ocaptchas/repo"
+	"github.com/marcsj/ocaptchas/server"
 	"github.com/namsral/flag"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
 	"log"
+	"net"
 )
 
 var (
 	databaseFile = flag.String(
 		"database_file", "stored.db", "location for database file")
+	grpcPort = flag.String(
+		"grpc_port", "50051", "port for grpc connections")
 )
 func main() {
+	errChannel := make(chan error)
+
 	db, err := gorm.Open("sqlite3", *databaseFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	_, err = repo.NewSessionRepo(db)
+	sessionRepo, err := repo.NewSessionRepo(db)
 	if err != nil {
 		log.Fatal(err)
+	}
+	_, err = repo.NewChallengeQuestionsRepo(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	challengeController := controller.NewChallengeController(sessionRepo)
+
+	challengeServer := server.NewChallengeServer(challengeController)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", *grpcPort))
+	if err != nil {
+		log.Fatal(err)
+	}
+	grpcServer := grpc.NewServer()
+
+	challenge.RegisterChallengeServer(grpcServer, challengeServer)
+
+	// running gRPC server
+	go func () {
+		grpclog.Infof("Starting gRPC server. tcp port: %v", *grpcPort)
+		errChannel <- grpcServer.Serve(lis)
+	}()
+
+	log.Println("started ocaptchas server")
+
+	for {
+		select {
+		case err := <-errChannel:
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
